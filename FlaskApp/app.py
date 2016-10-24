9eefdba636c9a081886144a08d666c51521078fe
Indulekha ghandikota <Indulekha ghandikota>
2016-10-24 08:48:14 -0400

    Login

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -6,26 +6,20 @@ app = Flask(__name__)
 app.debug = True
 app.secret_key = '\xb2,\xe1\x06\xb1\x0f*X\x18\x9e\xf3\xa9`\x13\x05\xfcb\x9e#W\xea\xb6\xa4Q'
 
-conn = sqlite3.connect('databsae.db')
+conn = sqlite3.connect('database.db')
 print "Opened database successfully"
 
-#conn.execute('CREATE TABLE users (name TEXT, password TEXT)')
+#conn.execute('CREATE TABLE users (username TEXT, password TEXT)')
 print "Table created successfully"
 conn.close()
-
 
 ##login = Blueprint('login', __name__)
 
 app.register_blueprint(controllers.login)
 
-
-
 @app.route("/")
 def main():
 	return render_template('index.html')
-
-
-
 
 if __name__ == "__main__":
 	app.run()
--- FlaskApp/controllers/login.py
+++ FlaskApp/controllers/login.py
@@ -1,66 +1,77 @@
 from flask import render_template, flash, redirect
 from forms import LoginForm, RegistrationForm
 from flask import *
-from mysql import mysql
+import sqlite3
+
+sqlite_file = 'database.db'
+table_name = 'users'
+username_column = 'username'
+pword_column = 'password'
+
+conn = sqlite3.connect(sqlite_file, check_same_thread=False)
+c = conn.cursor()
 
 login = Blueprint('login', __name__)
 
 @login.route('/login', methods = ['GET', 'POST'])
 def login_route():
-	form = LoginForm()
-
+	if request.method == 'GET':
+		return render_template('login.html')
 	if request.method == 'POST':
-		query = "SELECT password FROM users WHERE username = \'{}\'".format(request.form['username'].lower())
-		cursor = mysql.connection.cursor()
-		cursor.execute(query)
-		if cursor.rowcount == 0:
-			#username not found
-			error_username = True
-		else:
-			#verify password is correct
-			password = cursor.fetchall()
-			print password[0][0]
-			a = str(password[0][0]).split('$')
-			salt = str(a[1])
-			algorithm = str(a[0])
-			print a
-			print str(algorithm)
-			m = hashlib.new(algorithm)
-			m.update(salt + str(request.form['password']))
-			password_hash = m.hexdigest()
-			db = "$".join([algorithm,salt,password_hash])
-			print db
-			print password[0][0]
-			if db == password[0][0]:
-				session['username'] = request.form['username']
-				if request.args.get('url'):
-					return redirect(request.args.get('url'))
-				else:
-					return redirect(url_for('main.main_route'))
-			else:
-				error_password = True
-	return render_template('login.html', title='Sign In', form=form)
+		username = request.form['username']
+		password = request.form['password']
+		print "hi"
+		print username
+		print password
+		c.execute("SELECT * FROM users")
+		print c.rowcount
+		if c.rowcount == 1:
+			print "hi"
+		return redirect('/home')
+		# query = "SELECT password FROM users WHERE username = \'{}\'".format(request.form['username'].lower())
+		# cursor = mysql.connection.cursor()
+		# cursor.execute(query)
+		# if cursor.rowcount == 0:
+		# 	#username not found
+		# 	error_username = True
+		# else:
+		# 	#verify password is correct
+		# 	password = cursor.fetchall()
+		# 	print password[0][0]
+		# 	a = str(password[0][0]).split('$')
+		# 	salt = str(a[1])
+		# 	algorithm = str(a[0])
+		# 	print a
+		# 	print str(algorithm)
+		# 	m = hashlib.new(algorithm)
+		# 	m.update(salt + str(request.form['password']))
+		# 	password_hash = m.hexdigest()
+		# 	db = "$".join([algorithm,salt,password_hash])
+		# 	print db
+		# 	print password[0][0]
+		# 	if db == password[0][0]:
+		# 		session['username'] = request.form['username']
+		# 		if request.args.get('url'):
+		# 			return redirect(request.args.get('url'))
+		# 		else:
+		# 			return redirect(url_for('main.main_route'))
+		# 	else:
+		# 		error_password = True
+	return render_template('home.html')
 
 @login.route('/register', methods = ['GET', 'POST'])
 def register_route():
 	if request.method == "GET":
-		print "fhsudhfsj"
-		form = RegistrationForm()
-		return render_template('register.html', title='Sign Up', form=form)
+		return render_template('register.html')
 	elif request.method == "POST":
-		print "POST"
+		username = request.form['username']
+		password = request.form['password']
+		c.execute("INSERT INTO users (username, password) values (?,?)",(username,password))
+		conn.commit()
+		return redirect('/home')
 
-		query = "INSERT into users (" + username + ", " + password + ")"
-		cursor = mysql.connection.cursor()
-		cursor.execute(query)
 	
 @login.route('/home', methods = ['GET','POST'])
 def home_route():
 	return render_template('index.html')
 
-@login.route('/test', methods = ['POST'])
-def registertest_route():
-	if request.method == "POST":
-		print "fsdfs"
-		if form.validate_on_submit():
-			return redirect('/index')
